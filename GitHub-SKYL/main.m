fa1bd14e14f9721c5ea8636e6c6f1b475de227a2
Tayos <274577676@qq.com>
2017-07-08 11:31:32 +0800

    添加基础工程

---
+++ GitHub-SKYL/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitHub-SKYL
+//
+//  Created by Tayos on 2017/7/8.
+//  Copyright © 2017年 Tayos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
